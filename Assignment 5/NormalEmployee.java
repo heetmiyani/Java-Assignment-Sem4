b3244dc78420bb0f3a20fc5ebdfa8574b84810ec
heetmiyani <124970580+heetmiyani@users.noreply.github.com>
2023-04-06 23:26:58 +0530

    Delete Assignment 5 directory

--- Assignment 5/NormalEmployee.java
+++
@@ -1,9 +0,0 @@
-public class NormalEmployee extends Employee {
-    public NormalEmployee(String name, String address, int basicSalary) {
-        super(name, address, basicSalary);
-    }
-
-    public double getMonthlySalary() {
-        return (double) basicSalary / 12;
-    }
-}
--- Assignment 5/Shape.java
+++
@@ -1,6 +0,0 @@
-public interface Shape {
-    float calculateShapeArea();
-    float calculateShapePerimeter();
-    float pi =3.14F;
-
-}
